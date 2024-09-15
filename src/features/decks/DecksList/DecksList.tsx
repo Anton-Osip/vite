0964f4a9a49be39faf1b5a3cf538f2ce660f173b
Anton-Osip <osipchika@gmail.com>
2024-09-15 16:28:17 +0300

    2.2 - Redux flow - Решение

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,26 @@
 import s from './DecksList.module.css'
+import { useEffect, useState } from 'react'
+import { Deck, decksAPI } from '../decks-api.ts'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectDecks } from '../decks-selectors.ts'
+import { DeckItem } from './DeckItem/DeckItem.tsx'
+import { setDecksAC } from '../decks-reducer.ts'
 
 export const DecksList = () => {
-  return <ul className={s.list}></ul>
+  const dispatch = useDispatch()
+  const decks: Deck[] = useSelector(selectDecks)
+
+  useEffect(() => {
+    decksAPI.fetchDecks().then((res) => {
+      dispatch(setDecksAC(res.data.items))
+    })
+  }, [dispatch])
+
+  return (
+    <ul className={s.list}>
+      {decks.map((d) => (
+        <DeckItem deck={d} key={d.id} />
+      ))}
+    </ul>
+  )
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,8 +1,45 @@
 import axios from 'axios'
 
+type Author = {
+  id: string
+  name: string
+}
+
+export type Deck = {
+  isFavorite: boolean
+  author: Author
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
+
+type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
+
+type FetchDecksResponse = {
+  items: Deck[]
+  pagination: Pagination
+  maxCardsCount: number
+}
+
 export const instance = axios.create({
   baseURL: 'https://api.flashcards.andrii.es',
   headers: {
     'x-auth-skip': true,
   },
 })
+
+export const decksAPI = {
+  fetchDecks: () => {
+    return instance.get<FetchDecksResponse>('/v2/decks')
+  },
+}
